b149de07185915913989136962fb322fd3006ce2
Aina-VM <ainaval25@gmail.com>
2025-03-21 20:19:43 +0100

    all css and html done

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtener el botón de hamburguesa y el menú
-    const hamburgerMenu = document.getElementById('hamburgerMenu');
-    const navMenu = document.getElementById('navMenu');
-  // Función para alternar el menú
-    hamburgerMenu.addEventListener('click', function() {
-      navMenu.classList.toggle('active');
-    });
+  // Obtener el botón de hamburguesa y el menú
+  const hamburgerMenu = document.getElementById('hamburgerMenu');
+  const navMenu = document.querySelector('.menu');
+// Función para alternar el menú
+  hamburgerMenu.addEventListener('click', function() {
+    navMenu.classList.toggle('active');
   });
+});
 
 // document.getElementById("contactButton").addEventListener("click", function() {
 //   window.location.href = "./pages/contact.html";
---
+++ pages/projectPage.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el botón de hamburguesa y el menú
+    const hamburgerMenu = document.getElementById('hamburgerMenu');
+    const navMenu = document.querySelector('.menu');
+  // Función para alternar el menú
+    hamburgerMenu.addEventListener('click', function() {
+      navMenu.classList.toggle('active');
+    });
+  });
